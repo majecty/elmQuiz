fe6460d0e804868cbef1f6c1dc4a990c52ae8a71
ParkJuhyeong <majecty@gmail.com>
2015-10-10 12:06:28 +0900

    Hide operator.

--- FragEgg.elm
+++ FragEgg.elm
@@ -5,6 +5,7 @@ import Graphics.Element as Element
 import Graphics.Input.Field as Field
 import Signal
 import Signal exposing ((<~), (~))
+import String
 import Time
 import Time exposing (Time)
 import Window
@@ -13,8 +14,10 @@ type alias Input = ( Float, Element, Time)
 type alias UI = {
   name: Element
 }
+type alias Equation = List String
 type alias State = {
   value: Float,
+  equation: Equation,
   ui: UI
 }
 
@@ -28,11 +31,26 @@ upstate (x, nameField, timeDelta) s =
         ui <- { name=nameField } }
 
 initState : State
-initState = { value=3, ui={name=Element.empty}}
+initState = { value=3, ui={name=Element.empty}, equation=["1", "*", "3"]}
+
+hideOperator : String -> String
+hideOperator input = case input of
+  "+" -> "ㅁ"
+  "-" -> "ㅁ"
+  "*" -> "ㅁ"
+  "/" -> "ㅁ"
+  _ -> input
+
+showEquation : Equation -> Element
+showEquation equation =
+  let showableTokens = List.map hideOperator equation
+  in
+     Element.show <| String.concat showableTokens
 
 view : (Int, Int) -> State -> Element
 view (w, h) s = 
-  let allElements = Element.flow Element.down [ Element.show s.value, s.ui.name ]
+  let allElements = Element.flow Element.down
+        [ Element.show s.value, showEquation s.equation, s.ui.name ]
       container = Element.container w h Element.middle allElements
       coloredContainer = Element.color Color.brown <| container
   in
